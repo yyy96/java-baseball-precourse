bb8f7322762f10bb876317813eeaef5102cb4bfc
yyy96 <dlduswo1247@gmail.com>
2021-12-16 12:55:42 +0900

    축약되어있던 변수명 수정 (수정)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,5 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -7,78 +8,76 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Application {
-    public static final int INPUT_LENGTH_BOUND =3;
+    public static final int INPUT_LENGTH_BOUND = 3;
     public static final int GAME_CONTINUE = 1;
     public static final int GAME_FINISH = 2;
+    public static final int CARD_COUNT = 3;
+    public static final int USED_NUMBER = 1;
+    public static final int ALL_STRIKE = 3;
+    public static final int NO_USED_CARD = -1;
 
     public static void main(String[] args) {
 
-        while(true){
-            List<Integer> randomNumber = new ArrayList<>(3);
-            //1. 컴퓨터가 랜덤 숫자를 정한다
-            randomNumber = computerPickNumber();
+        while (true) {
+            List<Integer> randomNumber = computerPickNumber();
 
-            //2. 플레이어가 숫자게임을 시작한다.
-            if(playGame(randomNumber)==GAME_CONTINUE){
+            if (playGame(randomNumber) == GAME_CONTINUE) {
                 continue;
-            }
-            else{
+            } else {
                 break;
             }
         }
 
     }
 
-    public static List<Integer> computerPickNumber(){
-        int cardCnt = 0;
-        int[] used = new int[10];
-        Arrays.fill(used,0);
-        List<Integer> computerNumber = new ArrayList<>(3);
+    public static List<Integer> computerPickNumber() {
+        int cardCount = 0;
+        int[] checkUsedUniqueNumber = new int[10];
+        Arrays.fill(checkUsedUniqueNumber, 0);
+        List<Integer> computerRandomNumber = new ArrayList<>(CARD_COUNT);
 
-        while(true){
-            int num = Randoms.pickNumberInRange(1,9);
-            if(used[num]==1) continue;
-            used[num] = 1;
-            cardCnt++;
-            computerNumber.add(num);
-            if(cardCnt==3) break;
-        }
-
-        return computerNumber;
-    }
-
-
-
-    public static int playGame(List<Integer> randomNumber){
-        //게임 진행
         while(true) {
-            List<Integer> playerNumber = new ArrayList<>(3);
-            playerNumber = playerInputNumber();
-
-            if(checkNumber(randomNumber, playerNumber)==true){
-                break;
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (checkUsedUniqueNumber[number] == USED_NUMBER) {
+                continue;
             }
-            else {
+            checkUsedUniqueNumber[number] = USED_NUMBER;
+            computerRandomNumber.add(number);
+            cardCount++;
+            if (cardCount == CARD_COUNT) break;
+        }
+
+        return computerRandomNumber;
+    }
+
+
+    public static int playGame(List<Integer> randomNumber) {
+        //게임 진행
+        while (true) {
+            List<Integer> playerNumber = playerInputNumber();
+
+            if (checkPlayerNumber(randomNumber, playerNumber) == true) {
+                break;
+            } else {
                 continue;
             }
         }
 
-        if(gameOver() == GAME_CONTINUE){
+        if (gameOver() == GAME_CONTINUE) {
             return GAME_CONTINUE;
-        }
-        else{
+        } else {
             return GAME_FINISH;
         }
     }
 
-    public static List<Integer> playerInputNumber(){
-        List<Integer> playerNumber = new ArrayList<>(3);
+    public static List<Integer> playerInputNumber() {
+        List<Integer> playerNumber = new ArrayList<>(CARD_COUNT);
         String input;
         System.out.print("숫자를 입력해주세요 : ");
         input = Console.readLine();
         for (int i = 0; i < 3; i++) {
-            int num = Character.getNumericValue(input.charAt(i));
-            playerNumber.add(num);
+            int number = Character.getNumericValue(input.charAt(i));
+            playerNumber.add(number);
         }
 
         validateNumberCheck(input);
@@ -87,65 +86,65 @@ public class Application {
 
 
     private static void validateNumberCheck(final String input) {
-        if (input.length()!=INPUT_LENGTH_BOUND) {
+        if (input.length() != INPUT_LENGTH_BOUND) {
             throw new IllegalArgumentException("numbers cannot be empty.");
         }
     }
 
-    public static int gameOver(){
+    public static int gameOver() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-        String gameOver = Console.readLine();
-        int gameOverNum = Character.getNumericValue(gameOver.charAt(0));
-        if(gameOverNum==GAME_CONTINUE){
+        String gameOverInput = Console.readLine();
+        int gameOver = Character.getNumericValue(gameOverInput.charAt(0));
+        if (gameOver == GAME_CONTINUE) {
             return GAME_CONTINUE;
-        }
-        else{
+        } else {
             return GAME_FINISH;
         }
     }
 
-    public static Boolean checkNumber(List<Integer> randomNumber, List<Integer> playerNumber){
-        int ball =0;
-        int strike =0;
+    public static Boolean checkPlayerNumber(List<Integer> randomNumber, List<Integer> playerNumber) {
+        int ball = 0;
+        int strike = 0;
         int[] randomNumberIndex = new int[10];
         randomNumberIndex = initRandomNumberIndex(randomNumber);
 
-        for(int i=0; i<3; i++){
-            if(randomNumberIndex[playerNumber.get(i)]==i) strike++;
-            else if(randomNumberIndex[playerNumber.get(i)]!=-1) ball++;
+        for (int i = 0; i < CARD_COUNT; i++) {
+            if (randomNumberIndex[playerNumber.get(i)] == i) {
+                strike++;
+            } else if (randomNumberIndex[playerNumber.get(i)] != NO_USED_CARD) {
+                ball++;
+            }
         }
 
-        //결과 출력
         printResult(ball, strike);
 
-        if(strike==3){
+        if (strike == ALL_STRIKE) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static int[] initRandomNumberIndex(List<Integer> randomNumber){
+    public static int[] initRandomNumberIndex(List<Integer> randomNumber) {
         int[] randomNumberIndex = new int[10]; //direct address table
-        Arrays.fill(randomNumberIndex,-1);
+        Arrays.fill(randomNumberIndex, NO_USED_CARD);
 
         //컴퓨터의 숫자와 index 번호를 저장한다
-        for(int i=0; i<3; i++){
+        for (int i = 0; i < CARD_COUNT; i++) {
             randomNumberIndex[randomNumber.get(i)] = i;
         }
         return randomNumberIndex;
     }
 
 
-    public static void printResult(int ball, int strike){
-        if(ball!=0 && strike!=0)
-            System.out.println(ball+"볼 "+strike+"스트라이크");
-        else if(ball!=0 && strike==0)
-            System.out.println(ball+"볼");
-        else if(ball==0 && strike!=0)
-            System.out.println(strike+"스트라이크");
+    public static void printResult(int ball, int strike) {
+        if (ball != 0 && strike != 0)
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        else if (ball != 0 && strike == 0)
+            System.out.println(ball + "볼");
+        else if (ball == 0 && strike != 0)
+            System.out.println(strike + "스트라이크");
         else
             System.out.println("낫싱");
     }
